92ba3ca7ac51edb6743dffa2e1016c0211a102f0
allobo49 <147623051+allobo49@users.noreply.github.com>
2024-05-15 18:18:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ def map_to_cefr(difficulty_level):
 # Streamlit app interface
 st.title('French4U 🇫🇷')
 st.header('Text Difficulty Predictor')
-st.write('Enter a French sentence below and click the button to analyze its difficulty.')
+st.write('Enter a French sentence below and click the button to analyze its difficulty. Note that if the sentence is not in French, the difficulty prediction will not be accurate.')
 
 user_input = st.text_area("Insert your text here", height=150)
 
